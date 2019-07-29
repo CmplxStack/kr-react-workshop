4910cc2da05a163abe0b6d0113a08f48826d0e73
Mike <Michael.Anthony.Stack@gmail.com>
2019-07-29 06:57:05 -0400

    TodoChallenge

--- Examples/PassClick/setup/src/App/Components/Counter/Counter.tsx
+++ Examples/PassClick/setup/src/App/Components/Counter/Counter.tsx
@@ -1,13 +1,22 @@
 import React, { Component } from "react";
 import { Button } from "./ButtonComponent";
 
-export default class Counter extends Component {
-  state = {
-    count: 0,
-  };
+interface ICounterState {
+  count: number;
+}
+export default class Counter extends Component<any, ICounterState> {
+  constructor(props: any) {
+    super(props);
+
+    this.state = {
+      count: 0,
+    };
+  }
+
   handleClick = () => {
     this.setState({ count: this.state.count + 1 });
   };
+
   render() {
     return (
       <div>
